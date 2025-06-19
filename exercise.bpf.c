b55f70dc810404a27eef9b9dcab78e2bedb4b6d7
Eyal Royee <eyal.royee@cs.huji.ac.il>
2025-06-19 16:07:08 +0300

    End of exercise submission.
    Implemented:
    1) logging metadata about outgoing tcp packets (through BPF_HASH).
    2) exporting suspected HTTP packets to user-psace (through
       BPF_PERF_OUTPUT).
    
    Signed-off-by: Eyal Royee <eyal.royee@cs.huji.ac.il>

--- exercise.bpf.c
+++ exercise.bpf.c
@@ -1,38 +1,107 @@
 #include <uapi/linux/ptrace.h>
 #include <net/sock.h>
 #include <bcc/proto.h>
-// #include <linux/in6.h>
 #include <linux/net.h>
 #include <linux/socket.h>
 #include <net/inet_sock.h>
+#include <net/sock.h>  /* for struct sock */
+
+#ifndef TRUE
+#define TRUE 1
+#endif
+
+#define HTTP_PORT 80
+#define MAX_PAYLOAD_LEN 256
 
 struct tcp_key_t {
-	u32 pid;
+        u32 pid;
 };
 
 struct tcp_value_t {
-	u32 packet_size;
+        u16 family;
+        u8 is_ipv4;
+        __be32 ipv4_src_addr;
+        __be32 ipv4_dst_addr;
+        u16 src_port;
+        u16 dst_port;
+        u32 packet_size;
+        char proc_name[TASK_COMM_LEN];
 };
 
 // accessible via BPF instance b like this: b.get_map("tcp_outboud_map")
 BPF_HASH(tcp_outboud_map, struct tcp_key_t, struct tcp_value_t);
 
-SEC("kretprobe/tcp_sendmsg")
-int BPF_KRETPROBE(kretprobe__tcp_sendmsg, struct pt_regs *ctx)
+struct event_t {
+        u32 pid;
+        u32 packet_size;
+        u8 payload[MAX_PAYLOAD_LEN + 1];
+};
+BPF_PERF_OUTPUT(deep_packets_inspect_events);
+
+
+// SEC("kprobe/tcp_sendmsg")
+// int BPF_KPROBE(kretprobe__tcp_sendmsg, struct pt_regs *ctx, struct sock *sk, struct msghdr *msg, size_t len)
+int kprobe__tcp_sendmsg(struct pt_regs *ctx, struct sock *sk, struct msghdr *msg, size_t len)
 {
-	struct tcp_value_t empty = {};
-	u32 pid = bpf_get_current_pid_tgid() >> 32;
-	int packet_size = PT_REGS_RC(ctx);
+        struct tcp_value_t empty = {};
+        u32 pid = bpf_get_current_pid_tgid() >> 32;
+        u16 dport;
 
-	struct tcp_key_t key = {
-		.pid = pid
-	};
+        struct tcp_key_t key = {
+                .pid = pid
+        };
 
-	struct tcp_value_t *value = tcp_outboud_map.lookup_or_init(&key, &empty);
+        struct tcp_value_t *value = tcp_outboud_map.lookup_or_init(&key, &empty);
 
-	value->packet_size = packet_size;
+        bpf_probe_read_kernel(&value->family, sizeof(value->family),  &sk->__sk_common.skc_family);
+        if (value->family == AF_INET) {
+                value->is_ipv4 = TRUE;
+                bpf_probe_read_kernel(&value->ipv4_src_addr, sizeof(value->ipv4_src_addr),
+                                        &sk->__sk_common.skc_rcv_saddr);
+                bpf_probe_read_kernel(&value->ipv4_dst_addr, sizeof(value->ipv4_src_addr),
+                                        &sk->__sk_common.skc_daddr);
+        }
 
-	tcp_outboud_map.update(&key, &value);
+        bpf_probe_read_kernel(&value->src_port, sizeof(value->ipv4_src_addr),
+                                &sk->__sk_common.skc_num);
+        bpf_probe_read_kernel(&dport, sizeof(dport),
+                                &sk->__sk_common.skc_dport);
+        value->dst_port = ntohs(dport);
 
-	return 0;
+        value->packet_size = len;
+        if (bpf_get_current_comm(&value->proc_name, TASK_COMM_LEN)) {
+                bpf_trace_printk("Failed to get comm\n");
+                return 0;
+        }
+
+        tcp_outboud_map.update(&key, value);
+
+        /* suspected as HTTP, export to user-space for inspection */
+        if (value->dst_port == HTTP_PORT || value->src_port == HTTP_PORT) {
+                struct event_t event;
+                struct iovec *iovp = NULL;
+                struct iovec iov;
+
+                bpf_probe_read_kernel(&iovp, sizeof(iovp), &msg->msg_iter.iov);
+                if (!iovp) {
+                        /* can't access iov data */
+                        return 0;
+                }
+
+                /* read first iov data block from userspace */
+                bpf_probe_read_kernel(&iov, sizeof(iov), iovp);
+                size_t to_copy = iov.iov_len;
+                if (to_copy > MAX_PAYLOAD_LEN) {
+                        to_copy = MAX_PAYLOAD_LEN;
+                }
+
+                bpf_probe_read_user(&event.payload, to_copy, iov.iov_base);
+                event.payload[MAX_PAYLOAD_LEN] = '\0';
+                event.pid = pid;
+                event.packet_size = to_copy;
+
+                deep_packets_inspect_events.perf_submit(ctx, &event, sizeof(event));
+        }
+
+        return 0;
 }
--- exercise.py
+++ exercise.py
@@ -3,6 +3,7 @@
 import time
 
 from datetime import datetime
+from functools import partial
 
 from bcc import BPF
 
@@ -12,25 +13,54 @@ BPF_FILENAME = "exercise.bpf.c"
 TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
 
 
+def print_http_pkt(cpu, data, size, b):
+    event = b["deep_packets_inspect_events"].event(data)
+
+    now = datetime.now()
+    formatted_time = now.strftime(TIME_FORMAT)
+
+    print(f"At {formatted_time} - pid: {event.pid} has sent the following data:")
+    print(f"\t Raw Data: {bytearray(event.payload)}")
+
+
 def main():
-    with open("BPF_FILENAME", "rt") as f:
+    with open(BPF_FILENAME, "rt") as f:
         bpf_code = f.read()
 
     b = BPF(text=bpf_code)
 
-    b.attach_kretprobe(event="tcp_sendmsg", fn_name="kretprobe__tcp_sendmsg")
+    b.attach_kprobe(event="tcp_sendmsg", fn_name="kprobe__tcp_sendmsg")
+    b["deep_packets_inspect_events"].open_perf_buffer(
+        partial(print_http_pkt, b=b)
+    )
 
     while True:
         try:
-            time.sleep(SLEEP_INTERVAL)
+            # time.sleep(SLEEP_INTERVAL)
 
+            # timeout is in miliseconds
+            b.perf_buffer_poll(timeout=SLEEP_INTERVAL * 1000)
+
+            """
             now = datetime.now()
             formatted_time = now.strftime(TIME_FORMAT)
 
-            # for key, value in b.get_table("tcp_outboud_map"):  # get_table seems depracted
-            for key, value in b["tcp_outboud_map"]:
-                print(f"At time: {formatted_time} - pid: {pid} sent {value.packet_size} bytes")
+            # for key, value in b.get_table("tcp_outboud_map").items():  # get_table seems depracted
+            for key, value in b["tcp_outboud_map"].items():
+                print(f"At time: {formatted_time} - process: {str(value.proc_name)} (pid: {key.pid}):")
+                print(f"\tSent {value.packet_size} bytes (Using IPv4: {bool(value.is_ipv4)})")
+                if value.is_ipv4:
+                    print(f"\tFrom: src ip: {value.ipv4_src_addr} and src port: {value.src_port}")
+                    print(f"\tTo: dst ip: {value.ipv4_dst_addr} and dst port: {value.dst_port}")
+                else:
+                    print(f"\tFrom: src port: {value.src_port}")
+                    print(f"\tTo: dst port: {value.dst_port}")
+                print("-" * 50)  # seperator
+            """
 
         except KeyboardInterrupt:
             print("Exiting")
             break
+
+if __name__ == "__main__":
+    main()
